5a4b0312b9c776e5ba398ccfdcb9ed1e50c768e0
Aayush Lamsal <lamsal.aayush123@gmail.com>
2024-04-11 14:52:00 +0545

    reformatted the code, managed the timing

--- Piano/main.py
+++ Piano/main.py
@@ -1,67 +1,73 @@
 import tkinter as tk
-from pyo import *
-import os
-import time
+from pyo import Server, Sine, Adsr
 import threading
+import time
+import os
 
+s = Server(nchnls=2, sr=44100, duplex=0).boot().start()
 
 def playsound(fq):
-    harmonics=5
-    s = Server(nchnls=2, sr=44100, duplex=0).boot().start()
+    harmonics = 5
 
-    f = Adsr(attack=.02, decay=.5, sustain=0.3, release=1, dur=2)
+    f = Adsr(attack=0.02, decay=0.9, sustain=0.3, release=0, dur=2)
+
     a = Sine(freq=fq, mul=f)
-    for i in range(0,harmonics+1):
-        a+=Sine(freq=fq*i, mul=f*0.2)
-    a.out()
-    f.play()
-    time.sleep(0.3)
-    s.stop()
+    for i in range(1, harmonics + 1):
+        a += Sine(freq=fq * i, mul=f * 0.1)
 
+    a.out()
+
+    f.play()
+
+    time.sleep(1.3)
 
 
 def displayblock(array):
     os.system('clear')
-    for i in range(10, 0, -1):
+    for i in range(8, 0, -1):
         for j in array:
             if i <= j:
-                print(chr(0x2593)*6, end='')
+                print(chr(0x2593) * 6, end='')
             else:
                 print('       ', end='')
-        time.sleep(0)
         print()
 
-
 keystatus = False
+data = [0] * 8
+keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k']
+threads = []
 
-data = [0]*10
-
-keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon']
-deltime=0
 def keydown(event):
-    global origin
-    origin=time.time()
+    global keystatus
+
     key = event.keysym
     keystatus = True
-    data = [0]*10
+    data = [0] * 8
 
-    while keystatus is True:
-        if key in keys:
-            k = keys.index(key)
-            data[k] = 8
-            displayblock(data)
-            playsound(200+(k)*101.9)
+    if key in keys:
+        k = keys.index(key)
+        frequency = 200 + (k) * 127.375
 
-        keystatus = root.bind('<ButtonRelease-1>', keyup)
+        data[k] = 8
+        displayblock(data)
 
+        thread = threading.Thread(target=playsound, args=(frequency,))
+        threads.append(thread)
+        thread.start()
 
 def keyup(event):
+    global keystatus
     keystatus = False
 
-
 root = tk.Tk()
-root.title("key sense!")
+root.title("Key Sense!")
+
 root.bind('<KeyPress>', keydown)
+root.bind('<KeyRelease>', keyup)
 
 root.mainloop()
 
+for thread in threads:
+    thread.join()
+
+s.stop()
